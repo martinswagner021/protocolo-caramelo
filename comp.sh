ea6a89e87a9f814793ac468c42e44c4433432841
Wagner <martinswagner021@gmail.com>
2025-06-17 22:20:49 -0300

    chore: remove input fixa

--- comp.sh
+++ comp.sh
@@ -2,5 +2,5 @@ gcc -o servidor.out servidor-tcp.c
 ./servidor.out &
 gcc -o cliente.out cliente-tcp.c
 # ./cliente.out TAM R IP|PORTA|NOME|
-./cliente.out < input.txt
+./cliente.out
 kill -s INT $(pgrep servidor.out)
